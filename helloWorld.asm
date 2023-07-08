a17c8afc19468e6eb49ad1772c31950926f8accd
Mart van der Zalm <martvanderzalm@gmail.com>
2023-07-08 22:22:03 +0200

    Commit

--- helloWorld.asm
+++ helloWorld.asm
@@ -1,41 +1,20 @@
-section .data
-	msg db "Hello World", 0x0A
-    	msgLength equ $-msg
+section .bss
 
-    	dValue dd 0
-    	bNum db 42
-    	wNum dw 5000
-    	dNum dd 73000
-    	qNum dq 73000000
-    	bAns db 0
-    	wAns dw 0
-    	dAns dd 0
-    	qAns dq 0
+
+section .data
+	msg: db "Hello World", 0x0A
+    	msgLength: equ $-msg
 
 section .text
 global _start
 
 _start:
-	mov dword [dValue], 27
-	
-	mov al, byte [bNum]
-	mov byte [bAns], al
+	mov rax, 1 		; system write    
+	mov rdi, 1 		; stdout
+	mov rsi, msg    	; message to write
+	mov rdx, msgLength	; message length
+	syscall         	; call kernel
 
-	mov ax, word [wNum]
-	mov word [wAns], ax
-
-	mov eax, dword [dNum]
-	mov dword [dAns], eax
-
-	mov rax, qword [qNum]
-	mov qword [qAns], rax
-
-	mov rax, 1      
-	mov rdi, 1      
-	mov rsi, msg    
-	mov rdx, msgLength
-	syscall         
-
-	mov rax, 60     
-	xor rdi, rdi    
-	syscall        
+	mov rax, 60     	; system exit
+	xor rdi, 0 		; error code 0 (success)
+	syscall        		; call kernel
